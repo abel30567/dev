ac22db32c8411c62c0f193df9338b2a395ebc087
abel30567 <abel@Abels-MacBook-Pro.local>
2017-03-25 12:04:06 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    home: './home/index.jsx',
+    product: './product/index.jsx'
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: "/dist/",
+    filename: '[name]-bundle.js',
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  }
+
+};
